273b5295f7a4ac235090aacdcab9068210a24488
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-05-09 15:43:32 +0100

    add new banner and logo, update css, add dropdown menu for category

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -51,87 +51,87 @@ const showAlert = (message, type) => {
 
 
 
-const viewBtn = document.getElementById("viewBtn");
+// const viewBtn = document.getElementById("viewBtn");
 
-viewBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    viewPosts();
-})
+// viewBtn.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     viewPosts();
+// })
 
-const viewPosts = async () => {
-    const response = await fetch("http://localhost:3000/posts");
-    const data = await response.json();
-    if (data.error) {
-        alert(data.error);
-    }
-    const container = document.getElementById("postDataContainer");
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
+// const viewPosts = async () => {
+//     const response = await fetch("http://localhost:3000/posts");
+//     const data = await response.json();
+//     if (data.error) {
+//         alert(data.error);
+//     }
+//     const container = document.getElementById("postDataContainer");
+//     while (container.firstChild) {
+//         container.removeChild(container.firstChild);
+//     }
 
-    data.forEach(post => {
-        let id = post.id;
-        const postDataElement = document.createElement("div");
+//     data.forEach(post => {
+//         let id = post.id;
+//         const postDataElement = document.createElement("div");
 
-        const titleElement = document.createElement("h2");
-        const dateElement = document.createElement("p");
-        const timeElement = document.createElement("p");
-        const categoryElement = document.createElement("p");
-        const imageElement = document.createElement("img");
-        const contentElement = document.createElement("p");
+//         const titleElement = document.createElement("h2");
+//         const dateElement = document.createElement("p");
+//         const timeElement = document.createElement("p");
+//         const categoryElement = document.createElement("p");
+//         const imageElement = document.createElement("img");
+//         const contentElement = document.createElement("p");
 
-        console.log(post);
+//         console.log(post);
 
-        postDataElement.classList.add("postDataContainer");
-        postDataElement.id = id;
-        titleElement.classList.add("title");
-        contentElement.classList.add("content");
-        categoryElement.classList.add("category");
-        dateElement.classList.add("category");
-        timeElement.classList.add("category");
-        imageElement.classList.add("image");
+//         postDataElement.classList.add("postDataContainer");
+//         postDataElement.id = id;
+//         titleElement.classList.add("title");
+//         contentElement.classList.add("content");
+//         categoryElement.classList.add("category");
+//         dateElement.classList.add("category");
+//         timeElement.classList.add("category");
+//         imageElement.classList.add("image");
 
-        titleElement.textContent = post.title;
-        dateElement.textContent = post.date;
-        timeElement.textContent = post.time;
-        contentElement.textContent = post.content;
-        categoryElement.textContent = post.category;
+//         titleElement.textContent = post.title;
+//         dateElement.textContent = post.date;
+//         timeElement.textContent = post.time;
+//         contentElement.textContent = post.content;
+//         categoryElement.textContent = post.category;
 
-        const buffer = new Uint8Array(post.image.data);
-        let binary = "";
-        for (let i = 0; i < buffer.length; i++) {
-            binary += String.fromCharCode(buffer[i]);
-        }
-        const base64 = btoa(binary);
-        const imageUrl = `data:image;base64,${base64}`;
-        imageElement.src = imageUrl;
+//         const buffer = new Uint8Array(post.image.data);
+//         let binary = "";
+//         for (let i = 0; i < buffer.length; i++) {
+//             binary += String.fromCharCode(buffer[i]);
+//         }
+//         const base64 = btoa(binary);
+//         const imageUrl = `data:image;base64,${base64}`;
+//         imageElement.src = imageUrl;
 
-        postDataElement.appendChild(titleElement);
-        postDataElement.appendChild(dateElement);
-        postDataElement.appendChild(timeElement);
-        postDataElement.appendChild(categoryElement);
-        postDataElement.appendChild(imageElement);
-        postDataElement.appendChild(contentElement);
-        container.appendChild(postDataElement);
+//         postDataElement.appendChild(titleElement);
+//         postDataElement.appendChild(dateElement);
+//         postDataElement.appendChild(timeElement);
+//         postDataElement.appendChild(categoryElement);
+//         postDataElement.appendChild(imageElement);
+//         postDataElement.appendChild(contentElement);
+//         container.appendChild(postDataElement);
 
-        const postBoxes = document.getElementById(id);
+//         const postBoxes = document.getElementById(id);
 
-        postBoxes.addEventListener("click", () => {
-            deletePost(id);
-        })
-    })
-}
+//         postBoxes.addEventListener("click", () => {
+//             deletePost(id);
+//         })
+//     })
+// }
 
 
-const deletePost = async (id) => {
-    const response = await fetch(`http://localhost:3000/posts/${id}`, {
-        method: "DELETE"
-    });
-    if (response.status === 204) {
-        alert("Deleted.")
-    }
-    console.log(response);
-}
+// const deletePost = async (id) => {
+//     const response = await fetch(`http://localhost:3000/posts/${id}`, {
+//         method: "DELETE"
+//     });
+//     if (response.status === 204) {
+//         alert("Deleted.")
+//     }
+//     console.log(response);
+// }
 
 
 
