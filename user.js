4b7a759fab577d1c0cc7a0692013501a8b800cfe
ABF22 <bofalade@gmail.com>
2023-05-10 09:20:25 +0100

    deleted signup folder

--- user.js
+++ user.js
@@ -1,19 +1,42 @@
-const createUserForm = document.querySelector('#createUserForm');
+const body = document.body;
+const input = document.getElementById('name');
 
-createUserForm.addEventListener('submit', (event) => {
+input.addEventListener('input', () => {
+  if (input.value.toLowerCase() === 'green') {
+    body.style.backgroundColor = '#8FBF69';
+  } else {
+    body.style.backgroundColor = '';
+  }
+});
+
+const createUserForm = document.getElementById('createUserForm');
+
+createUserForm.addEventListener('submit', async (event) => {
   event.preventDefault();
 
-  const formData = new FormData(createUserForm);
+  const name = document.getElementById('name').value;
+  const postcode = document.getElementById('postcode').value;
+  const password = document.getElementById('password').value;
+  const profilePic = document.getElementById('profilePic').files[0];
 
-  fetch('/api/users', {
+  const formData = new FormData();
+  formData.append('name', name);
+  formData.append('postcode', postcode);
+  formData.append('password', password);
+  formData.append('profilePic', profilePic);
+
+  const response = await fetch('/api/users', {
     method: 'POST',
     body: formData
-  })
-  .then(response => {
-    alert('User created successfully!');
-    createUserForm.reset();
-  })
-  .catch(error => {
-    console.error(error);
   });
+
+  if (response.ok) {
+    const user = await response.json();
+    console.log(user);
+    // Do something with the created user
+  } else {
+    const error = await response.text();
+    console.error(error);
+    // Handle the error
+  }
 });
